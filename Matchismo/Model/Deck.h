bcff9fabc32f62c89f2722f951f24f8e44086bac
Carlos Aguayo <carlos.aguayo@gmail.com>
2013-10-19 01:04:27 -0400

    before assignment #1

---
+++ Matchismo/Model/Deck.h
@@ -0,0 +1,17 @@
+//
+//  Deck.h
+//  Matchismo
+//
+//  Created by Carlos Aguayo on 10/19/13.
+//  Copyright (c) 2013 Dreamskiale. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Card.h"
+
+@interface Deck : NSObject
+
+- (void)addCard:(Card *)card atTop:(BOOL)atTop;
+- (Card *)drawRandomCard;
+
+@end
---
+++ Matchismo/Model/PlayingCardDeck.h
@@ -0,0 +1,13 @@
+//
+//  CardGameDeck.h
+//  Matchismo
+//
+//  Created by Carlos Aguayo on 10/19/13.
+//  Copyright (c) 2013 Dreamskiale. All rights reserved.
+//
+
+#import "Deck.h"
+
+@interface PlayingCardDeck : Deck
+
+@end
