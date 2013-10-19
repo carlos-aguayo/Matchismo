3439dfeb73bf112c5fbb0f33d560c24edb405c3f
Carlos Aguayo <carlos.aguayo@gmail.com>
2013-10-18 23:18:52 -0400

    Initial Commit

---
+++ Matchismo/CardGameViewController.h
@@ -0,0 +1,13 @@
+//
+//  CardGameViewController.h
+//  Matchismo
+//
+//  Created by Carlos Aguayo on 10/18/13.
+//  Copyright (c) 2013 Dreamskiale. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameViewController : UIViewController
+
+@end
---
+++ Matchismo/CardGameViewController.m
@@ -0,0 +1,29 @@
+//
+//  CardGameViewController.m
+//  Matchismo
+//
+//  Created by Carlos Aguayo on 10/18/13.
+//  Copyright (c) 2013 Dreamskiale. All rights reserved.
+//
+
+#import "CardGameViewController.h"
+
+@interface CardGameViewController ()
+
+@end
+
+@implementation CardGameViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
