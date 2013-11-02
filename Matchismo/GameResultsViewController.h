ae1bd89d3f73f38a3008729c016f9bf222fe064e
Carlos Aguayo <carlos.aguayo@gmail.com>
2013-11-01 22:48:45 -0400

    added view controller

---
+++ Matchismo/GameResultsViewController.h
@@ -0,0 +1,13 @@
+//
+//  GameResultsViewController.h
+//  Matchismo
+//
+//  Created by Carlos Aguayo on 10/25/13.
+//  Copyright (c) 2013 Dreamskiale. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GameResultsViewController : UIViewController
+
+@end
