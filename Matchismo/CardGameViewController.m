179f4e7d78585b995c7639e1b654fa2758453f65
Carlos Aguayo <carlos.aguayo@gmail.com>
2014-04-27 11:41:03 -0400

    flips once

--- Matchismo/CardGameViewController.m
+++ Matchismo/CardGameViewController.m
@@ -14,16 +14,11 @@
 
 @implementation CardGameViewController
 
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)touchCardButton:(UIButton *)sender {
+    
+    [sender setBackgroundImage:[UIImage imageNamed:@"cardback"]
+                      forState:UIControlStateNormal];
+    [sender setTitle:@"" forState:UIControlStateNormal];
 }
 
 @end
